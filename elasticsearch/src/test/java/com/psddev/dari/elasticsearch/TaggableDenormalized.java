ccea2318aee010ce3ac7451a285a458b1cc61891
billbell <billnbell@gmail.com>
2017-03-26 22:06:43 -0600

    Since moved to test Module, delete them.

--- elasticsearch/src/test/java/com/psddev/dari/elasticsearch/TaggableDenormalized.java
+++
@@ -1,6 +0,0 @@
-package com.psddev.dari.elasticsearch;
-
-import com.psddev.dari.db.Recordable;
-
-public interface TaggableDenormalized extends Recordable {
-}
--- elasticsearch/src/test/java/com/psddev/dari/elasticsearch/TaggableEmbedded.java
+++
@@ -1,6 +0,0 @@
-package com.psddev.dari.elasticsearch;
-
-import com.psddev.dari.db.Recordable;
-
-public interface TaggableEmbedded extends Recordable {
-}
